702984ccb6b098e9ff95f483fdb4ecee1b17af12
Bednarchuk Ivan-Vitaliy <xyjiigah.ib@gmail.com>
2022-04-10 17:02:37 +0200

    Create texts.py

---
+++ texts.py
@@ -0,0 +1,19 @@
+start_text = {
+    'ru': '/help - список команд\n/info - информация o боте\n/language - установить язык\n/support - поддержать автора бота', 
+    'en': '/help - list of commands\n/info - bot information\n/language - to set a language\n/support - support bot author'
+    }
+    
+help_text = {
+    'ru': 'Здесь у вас есть список команд:\n/time — текущее время на планетах\n/search — поиск типа миссий разлома бездны\n/sortie — миссии вылазки\n/trader — информация о торговце бездны\n/arbitration — текущея миссиия арбитража\n/nightwave - миссии ночной волны\n/events - ивенты', 
+    'en': 'Here you have a list of commands:\n/time - current time on planets\n/search - search for void fissure missions type\n/sortie - sortie missions\n/trader - void trader information\n/arbitration - current arbitration mission\n/nightwave - nightwave missions\n/events - events'
+    }
+
+description_text = {
+    'ru': 'Услуги бота:\n1 - предоставляет вам всю доступную информацию\n\nВеселитесь, Тэнно!', 
+    'en': 'Bot Services:\n1 - provides you with all available information\n\nHave fun, Tenno!'
+    }
+
+error_text = {
+    'ru': 'Что-то пошло не так. Пожалуйста, попытайтесь еще раз.', 
+    'en': 'Something went wrong please try again.'
+}
