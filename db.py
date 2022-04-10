97fb0d838dbedf8662abb35286ab3c58aef5fff4
Bednarchuk Ivan-Vitaliy <xyjiigah.ib@gmail.com>
2022-04-10 17:47:23 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import os
 from pymongo import MongoClient
 
-client = MongoClient(os.environ.get(MONGODB_URL))
+client = MongoClient(os.environ.get("MONGODB_URL"))
 db = client['t_bot']
 users_collection = db['users']
