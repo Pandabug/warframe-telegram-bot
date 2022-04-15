5480a7050ee70efdb5bd3319c1d47a7b69b21822
Bednarchuk Ivan-Vitaliy <xyjiigah.ib@gmail.com>
2022-04-15 23:26:26 +0200

    Update texts.py

--- texts.py
+++ texts.py
@@ -4,8 +4,8 @@ start_text = {
     }
     
 help_text = {
-    'ru': 'Здесь у вас есть список команд:\n/time — текущее время на планетах\n/search — поиск типа миссий разлома бездны\n/sortie — миссии вылазки\n/trader — информация о торговце бездны\n/arbitration — текущея миссиия арбитража\n/nightwave - миссии ночной волны\n/events - ивенты', 
-    'en': 'Here you have a list of commands:\n/time - current time on planets\n/search - search for void fissure missions type\n/sortie - sortie missions\n/trader - void trader information\n/arbitration - current arbitration mission\n/nightwave - nightwave missions\n/events - events'
+    'ru': 'Здесь у вас есть список команд:\n/help - список команд\n/info - информация o боте\n/language - установить язык\n/creator - создатель бота\n/time — текущее время на планетах\n/search — поиск типа миссий разлома бездны\n/sortie — миссии вылазки\n/trader — информация о торговце бездны\n/arbitration — текущея миссиия арбитража\n/nightwave - миссии ночной волны\n/events - ивенты', 
+    'en': 'Here you have a list of commands:\n/help - list of commands\n/info - bot information\n/language - to set a language\n/creator - bot creator\n/time - current time on planets\n/search - search for void fissure missions type\n/sortie - sortie missions\n/trader - void trader information\n/arbitration - current arbitration mission\n/nightwave - nightwave missions\n/events - events'
     }
 
 description_text = {
