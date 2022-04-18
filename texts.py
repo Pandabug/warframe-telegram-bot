6a05bc5032fd08e14022380219e328d8b55ac98f
Bednarchuk Ivan-Vitaliy <xyjiigah.ib@gmail.com>
2022-04-18 13:35:00 +0200

    Update texts.py

--- texts.py
+++ texts.py
@@ -1,17 +1,40 @@
-start_text = {
-    'ru': '/help - список команд\n/info - информация o боте\n/language - установить язык\n/creator - создатель бота', 
-    'en': '/help - list of commands\n/info - bot information\n/language - to set a language\n/creator - bot creator'
-    }
-    
 help_text = {
-    'ru': 'Здесь у вас есть список команд:\n/help - список команд\n/info - информация o боте\n/language - установить язык\n/creator - создатель бота\n/time — текущее время на планетах\n/search — поиск типа миссий разлома бездны\n/sortie — миссии вылазки\n/trader — информация о торговце бездны\n/arbitration — текущея миссиия арбитража\n/nightwave - миссии ночной волны\n/events - ивенты', 
-    'en': 'Here you have a list of commands:\n/help - list of commands\n/info - bot information\n/language - to set a language\n/creator - bot creator\n/time - current time on planets\n/search - search for void fissure missions type\n/sortie - sortie missions\n/trader - void trader information\n/arbitration - current arbitration mission\n/nightwave - nightwave missions\n/events - events'
-    }
+    'ru': '''
+Здесь у вас есть список команд:
+/help - список команд
+/info - информация o боте
+/language - установить язык
+/creator - создатель бота
+/time — текущее время на планетах
+/search — поиск типа миссий разлома бездны
+/sortie — миссии вылазки
+/trader — информация о торговце бездны
+/arbitration — текущея миссиия арбитража
+/nightwave - миссии ночной волны
+/events - ивенты
+/spreward - еженедельная награда теншина
+        ''', 
+    'en': """
+Here you have a list of commands:
+/help - list of commands
+/info - bot information
+/language - to set a language
+/creator - bot creator
+/time - current time on planets
+/search - search for void fissure missions type
+/sortie - sortie missions
+/trader - void trader information
+/arbitration - current arbitration mission
+/nightwave - nightwave missions
+/events - events
+/spreward - weekly tenshin reward
+        """
+}
 
 description_text = {
     'ru': 'Услуги бота:\n1 - предоставляет вам всю доступную информацию\n\nВеселитесь, Тэнно!', 
     'en': 'Bot Services:\n1 - provides you with all available information\n\nHave fun, Tenno!'
-    }
+}
 
 error_text = {
     'ru': 'Что-то пошло не так. Пожалуйста, попытайтесь еще раз.', 
@@ -27,7 +50,8 @@ mission_text = {
         4 : 'Враги', 
         5 : 'Ровнины Ейдолона', 
         6 : 'Валлис', 
-        7 : 'Камбион'
+        7 : 'Камбион',
+        8 : 'Выберете между',
     },
     'en': {
         0 : 'Planet', 
@@ -37,7 +61,8 @@ mission_text = {
         4 : 'Enemys', 
         5 : 'Plains of Eidolon', 
         6 : 'Orb Vallis', 
-        7 : 'Cambion Drift'
+        7 : 'Cambion Drift',
+        8 : 'Select between',
     }
 }
 
@@ -74,5 +99,22 @@ mission_names = {
     }
 }
 
+relic_names = {
+    'ru': {
+        0 : 'Лит',
+        1 : 'Мезо',
+        2 : 'Нео',
+        3 : 'Акси',
+        4 : 'Реквием',
+    },
+    'en': {
+        0 : 'Lith',
+        1 : 'Meso',
+        2 : 'Neo',
+        3 : 'Axi',
+        4 : 'Requiem',
+    }
+}
+
 creator_text = ['support', 'github', 'telegram']
-en_help_names = ['/time', '/search', '/sortie', '/trader', '/arbitration', '/nightwave', '/events']
+en_help_names = ['/time', '/search', '/sortie', '/trader', '/arbitration', '/nightwave', '/events', '/spreward']
